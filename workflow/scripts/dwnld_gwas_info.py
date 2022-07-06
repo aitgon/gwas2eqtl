c0773bebdd9b638064beb8a2c6fb53d9a87b5924
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2022-07-06 11:00:41 +0200

    ENH

--- workflow/scripts/dwnld_gwas_info.py
+++ workflow/scripts/dwnld_gwas_info.py
@@ -38,6 +38,7 @@ traits_exclude_df = pandas.read_csv("config/exclude_traits.txt", sep="\t", heade
 for rowi, row in traits_exclude_df.iterrows():
   trait_prefix = row[0]
   df.loc[df['trait'].str.startswith(trait_prefix), 'drop'] = 1
+df = df.loc[df['drop'] == 0]
 
 #%%
 exclude_datasets_df = pandas.read_csv("config/exclude_datasets.txt", sep="\t", header=None)
@@ -46,18 +47,29 @@ exclude_datasets_df = pandas.read_csv("config/exclude_datasets.txt", sep="\t", h
 for rowi, row in exclude_datasets_df.iterrows():
   dataset_prefix = row[0]
   df.loc[df['id'].str.startswith(dataset_prefix), 'drop'] = 1
-
-#%%
 df = df.loc[df['drop'] == 0]
 
 #%% select 1
-n = 25000
-ncontrol = 5000
-ncase = 5000
+n = 200000
+ncontrol = 40000
+ncase = 40000
 df = df.loc[df['population'] == 'European']  # keep european
 df = df.loc[df['ncontrol'] > ncontrol]  # ncontrol > 10000
 df = df.loc[df['ncase'] > ncase]  # ncase > 10000
 df = df.loc[(df['ncontrol'] + df['ncase']) > n]  # sum > 50000
+
+#%% drop if check url exists
+url_fmt = "https://gwas.mrcieu.ac.uk/files/{id}/{id}.vcf.gz.tbi"
+for rowi, row in df.iterrows():
+  Logger.debug(rowi)
+  id = row['id']
+  url = url_fmt.format(id=id)
+  r = requests.head(url)
+  if r.status_code != 200:
+    df.loc[rowi, 'drop'] = 1
+
+df = df.loc[df['drop'] == 0]
+df.drop(['drop'], axis=1, inplace=True)
 
 #%%
 annot_df = pandas.read_excel('config/manual_annotation.ods', engine='odf')
@@ -65,6 +77,11 @@ m_df = df.merge(annot_df[['id', 'manual_category']],  on='id', how='left')
 m_df.drop_duplicates(subset=m_df.columns.tolist(), inplace=True)
 
 #%%
-m_df = m_df[['id', 'manual_category', 'subcategory', 'trait', 'note', 'group_name', 'mr', 'year', 'author', 'sex', 'pmid', 'population', 'unit', 'sample_size', 'nsnp', 'build', 'category', 'subcategory', 'ontology', 'ncase', 'consortium', 'ncontrol', 'priority', 'sd', 'drop', 'manual_category']]
-gwassinfo_tsv_path = os.path.join(outdir_path, "gwasinfo_select_ncontrol{}_ncase{}_n{}.tsv".format(ncontrol, ncase, n))
-m_df.to_csv(gwassinfo_tsv_path, sep="\t", header=True, index=False)
+m_df = m_df[['id', 'manual_category', 'subcategory', 'trait', 'note', 'group_name', 'mr', 'year', 'author', 'sex', 'pmid', 'population', 'unit', 'sample_size', 'nsnp', 'build', 'category', 'subcategory', 'ontology', 'ncase', 'consortium', 'ncontrol', 'priority', 'sd', 'manual_category']]
+# gwassinfo_tsv_path = os.path.join(outdir_path, "gwasinfo_select_ncontrol{}_ncase{}_n{}.tsv".format(ncontrol, ncase, n))
+# m_df.to_csv(gwassinfo_tsv_path, sep="\t", header=True, index=False)
+
+#%%
+gwassinfo_ods_path = os.path.join(outdir_path, "gwasinfo_select_ncontrol{}_ncase{}_n{}.ods".format(ncontrol, ncase, n))
+with pandas.ExcelWriter(gwassinfo_ods_path) as fout:
+  df.to_excel(fout, index=False)
