a975fb8a685efcf30566baaba5b7a7ee94cd900c
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2022-09-07 06:57:15 +0200

    ENH stop silent run_coloc if no tophits

--- workflow/scripts/run_coloc.R
+++ workflow/scripts/run_coloc.R
@@ -58,6 +58,14 @@ out_df = data.frame(matrix(vector(), 0, length(out_cols), dimnames = list(c(), o
 ################################################################################
 # Load tophits
 tophits_df = read.table(tophits_tsv_path, sep = '\t', header = T)
+
+if (nrow(df) == 0) {  # not tophits
+  write.table(out_df, out_tsv_path, quote = F, sep = "\t", row.names = F, append = F, col.names = T)
+  opt <- options(show.error.messages = FALSE)
+  on.exit(options(opt))
+  stop()
+}
+
 tophits_df = tophits_df %>% dplyr::rename(chrom = chr, pos = position, ref = nea, alt = ea, maf = eaf)  # rename maf column
 
 ################################################################################
