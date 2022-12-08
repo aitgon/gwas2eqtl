b16c3c788c57c706f1bdc9ed7c5b5042da634819
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2022-12-08 14:24:30 +0100

    ENH

--- README2.md
+++ README2.md
@@ -8,4 +8,4 @@ docker compose --project-name gwas2eqtl_dev -f docker-compose.yml up --build --f
 
 python workflow/scripts/insrt_tophits.py postgresql://postgres:postgres@0.0.0.0:5437/gwas2eqtl config/gwas418.ods  out/gwas418/tophits/{gwas_id}/pval_5e-08/r2_0.1/kb_1000/hg38.tsv
 
-python workflow/scripts/insrt_coloc.py postgresql://postgres:postgres@0.0.0.0:5437/gwas2eqtl config/gwas_ieu-a-1162.ods config/eqtl_Schmiedel_2018_CD8_T-cell_naive.tsv out/gwas418/coloc/{gwas_id}/pval_5e-08/r2_0.1/kb_1000/window_1000000/{eqtl_id}.tsv
+python workflow/scripts/insrt_coloc.py postgresql://postgres:postgres@0.0.0.0:5437/gwas2eqtl config/gwas418.ods /home/gonzalez/Software/public/raw.githubusercontent.com/eQTL-Catalogue/eQTL-Catalogue-resources/master/tabix/tabix_ftp_paths.tsv out/gwas418/coloc/{gwas_id}/pval_5e-08/r2_0.1/kb_1000/window_1000000/{eqtl_id}.tsv
--- workflow/scripts/insrt_coloc.py
+++ workflow/scripts/insrt_coloc.py
@@ -34,26 +34,35 @@ eqtl_identifier_lst = (eqtl_df['ftp_path'].str.replace('.all.tsv.gz', '', regex=
 engine = create_engine(url)
 Base.metadata.create_all(engine)
 
+coloc_tab = Base.metadata.tables['coloc']
+stmt = coloc_tab.delete()
+engine.execute(stmt)
+
 gwas_eqtl_counter = 0
 gwas_counter = 0
 for gwas_id in gwas_identifier_lst:
+    concat_df = pandas.DataFrame()
     if gwas_counter % 10 == 0:
         print("GWAS counter: " + str(gwas_counter))
     for eqtl_id in eqtl_identifier_lst:
+        if gwas_eqtl_counter % 100 == 0:
+            print("\tGWAS/eQTL counter: " + str(gwas_eqtl_counter))
         coloc_tsv_path = coloc_path_strf.format(**{'gwas_id': gwas_id, 'eqtl_id': eqtl_id})
         if os.path.isfile(coloc_tsv_path):
-            df = pandas.read_csv(coloc_tsv_path, sep="\t")
-            df.columns = [c.lower() for c in df.columns]  # change to lower case
-            df.columns = [c.replace('.', '_') for c in df.columns]  # replace dots with underline
-            df.sort_values(by='pp_h4_abf', inplace=True, ascending=False)  # keep coloc with highest pp_h4_abf
-            df.drop_duplicates(['chrom', 'pos', 'alt', 'eqtl_gene_id', 'gwas_id', 'eqtl_id'], inplace=True)
-            df['rsid'] = df['rsid'].str.replace('rs', '').astype(int)
-            df_index = df['chrom'].astype(str) + "_" + df['pos'].astype(str) + "_" + df['eqtl_gene_id'] + "_" + df['gwas_id'] + "_" + df['eqtl_id']
-            df.set_index(df_index, inplace=True, verify_integrity=True)
-            df.index.rename('id', inplace=True)
-            # Delete and insert coloc data
-            coloc_tab = Base.metadata.tables['coloc']
-            stmt = coloc_tab.delete().where(coloc_tab.c.gwas_id==gwas_id).where(coloc_tab.c.eqtl_id==eqtl_id)
-            engine.execute(stmt)
-            df = df[[c.key for c in coloc_tab.columns][1:]]
-            df.to_sql('coloc', con=engine, if_exists='append', index=True, index_label='id')
+            coloc_df = pandas.read_csv(coloc_tsv_path, sep="\t")
+            coloc_df.columns = [c.lower() for c in coloc_df.columns]  # change to lower case
+            coloc_df.columns = [c.replace('.', '_') for c in coloc_df.columns]  # replace dots with underline
+            coloc_df.sort_values(by='pp_h4_abf', inplace=True, ascending=False)  # keep coloc with highest pp_h4_abf
+            coloc_df.drop_duplicates(['chrom', 'pos', 'alt', 'eqtl_gene_id', 'gwas_id', 'eqtl_id'], inplace=True)
+            coloc_df['rsid'] = coloc_df['rsid'].str.replace('rs', '').astype(int)
+            df_index = coloc_df['chrom'].astype(str) + "_" + coloc_df['pos'].astype(str) + "_" + coloc_df['eqtl_gene_id'] + "_" + coloc_df['gwas_id'] + "_" + coloc_df['eqtl_id']
+            coloc_df.set_index(df_index, inplace=True, verify_integrity=True)
+            coloc_df.index.rename('id', inplace=True)
+            concat_df = pandas.concat([concat_df, coloc_df], axis=0)
+        gwas_eqtl_counter = gwas_eqtl_counter + 1
+        # Delete and insert coloc data
+    if len(concat_df.columns) > 0:
+        concat_df = concat_df[[c.key for c in coloc_tab.columns][1:]]
+        print('insert', concat_df.shape)
+        concat_df.to_sql('coloc', con=engine, if_exists='append', index=True, index_label='id')
+    gwas_counter = gwas_counter + 1
